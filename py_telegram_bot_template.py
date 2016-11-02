fe56ecd95a2211608cb3a80cbe486cae594e54b0
Erika Vale <meadowdrone@gmail.com>
2016-11-02 07:04:14 -0400

    Condensed

--- py_telegram_bot_template.py
+++ py_telegram_bot_template.py
@@ -14,7 +14,33 @@ def main():
         LAST_UPDATE_ID = None
 
     while True:
-        brain(bot)
+        for update in bot.getUpdates(offset=LAST_UPDATE_ID, timeout=20):
+            try:
+                if update.message:
+                    if update.message.text:
+                        # The ID of the chat message received
+                        chat_id = update.message.chat_id
+                    
+                        # The content of the text received from the chat
+                        text = update.message.text.encode("utf-8")
+                        
+                        # The first name of the user sending the message
+                        first_name = update.message.from_user.first_name.encode("utf-8")
+                    
+                        # Makes the bot send a "bot is typing..." action
+                        bot.sendChatAction(
+                                chat_id,
+                                action=telegram.ChatAction.TYPING)
+
+                        # Post a message
+                        bot.sendMessage(update.message.chat_id, 
+                                "Hi %s, you wrote:\n%s" % (first_name, text))
+
+            except Exception as e:
+                print("Error occurred:\n%s" % e)
+            finally:
+                # Updates global offset to get the new updates
+                LAST_UPDATE_ID = update.update_id + 1
 
 
 def brain(bot):
@@ -22,33 +48,7 @@ def brain(bot):
     global LAST_UPDATE_ID
 
     # Request updates after the last updated_id
-    for update in bot.getUpdates(offset=LAST_UPDATE_ID, timeout=20):
-        try:
-            if update.message:
-                if update.message.text:
-                    # The ID of the chat message received
-                    chat_id = update.message.chat_id
-                
-                    # The content of the text received from the chat
-                    text = update.message.text.encode("utf-8")
-                    
-                    # The first name of the user sending the message
-                    first_name = update.message.from_user.first_name.encode("utf-8")
-                
-                    # Makes the bot send a "bot is typing..." action
-                    bot.sendChatAction(
-                            chat_id,
-                            action=telegram.ChatAction.TYPING)
 
-                    # Post a message
-                    bot.sendMessage(update.message.chat_id, 
-                            "Hi %s, you wrote:\n%s" % (first_name, text))
-
-        except Exception as e:
-            print("Error occurred:\n%s" % e)
-        finally:
-            # Updates global offset to get the new updates
-            LAST_UPDATE_ID = update.update_id + 1
 
 if __name__ == '__main__':
     main()
